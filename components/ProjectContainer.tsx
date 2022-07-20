3135c6baec03034f5b7adc1f0420389db93ea174
William Robersone <wmrobersone@yahoo.com>
2022-07-19 20:45:41 -0500

    update image

--- components/ProjectContainer.tsx
+++ components/ProjectContainer.tsx
@@ -19,31 +19,6 @@ export const ProjectContainer = () => {
       />
 
       <Project
-        title="Find Your Ingredients"
-        image="/static/projects/p2.jpg"
-        website="https://findingredients.netlify.app/"
-        video={false}
-        frontend="https://github.com/wrobersone/makeameal"
-        backend={false}
-        repo={false}
-        description="Consumes a 3rd Party REST API, fetching food ingredients using Vanilla JS"
-        tech={['HTML', 'CSS', 'JavaScript', 'REST']}
-      />
-
-      <Project
-        title="FLEX Fitness"
-        image="/static/projects/p3.jpg"
-        repo={false}
-        description="A responsive Fitness Website built in HTML5, CSS3, and JS with GSAP Animations."
-        tech={['HTML5', 'CSS3', 'JavaScript', 'GSAP']}
-        frontend="https://github.com/wrobersone/gsap-fitness"
-        backend={false}
-        video={false}
-        website="https://gsapfitness.netlify.app/"
-      />
-
-
-      <Project
         title="Movie Design App"
         image="/static/projects/p4.jpg"
         repo={false}
@@ -53,6 +28,43 @@ export const ProjectContainer = () => {
         backend={false}
         video={false}
         website="https://movies-app-silk.vercel.app/"
+      />
+
+      <Project
+        title="E-commerce: WWR Headphones"
+        image="/static/projects/p8.png"
+        repo={false}
+        description="A Modern Full Stack E-Commerce React Application with Stripe"
+        tech={['React', 'NextJS', 'Sanity', 'Stripe API']}
+        frontend="https://github.com/wrobersone/e-commerce_stripe"
+        backend="https://www.sanity.io/manage"
+        video={false}
+        website="https://e-commerce-stripe.vercel.app/"
+      />
+
+
+      <Project
+        title="FLEX Fitness"
+        image="/static/projects/p9.png"
+        repo={false}
+        description="A responsive Fitness Website built in HTML5, CSS3, and JS with GSAP Animations."
+        tech={['HTML5', 'CSS3', 'JavaScript', 'GSAP']}
+        frontend="https://github.com/wrobersone/gsap-fitness"
+        backend={false}
+        video={false}
+        website="https://gsapfitness.netlify.app/"
+      />
+
+      <Project
+        title="Find Your Ingredients"
+        image="/static/projects/p2.jpg"
+        website="https://findingredients.netlify.app/"
+        video={false}
+        frontend="https://github.com/wrobersone/makeameal"
+        backend={false}
+        repo={false}
+        description="Consumes a 3rd Party REST API, fetching food ingredients using Vanilla JS"
+        tech={['HTML', 'CSS', 'JavaScript', 'REST']}
       />
 
       <Project
@@ -83,7 +95,7 @@ export const ProjectContainer = () => {
         title="Zay Marie Music"
         image="/static/projects/p7.jpg"
         repo={false}
-        description="Designed a landing page for an independent music artist."
+        description="Music Landing Page for an R&B Independent music artist."
         tech={['HTML', 'CSS', 'Bootstrap', 'JavaScript', 'Ruby']}
         frontend={false}
         backend={false}
@@ -103,17 +115,7 @@ export const ProjectContainer = () => {
         website="https://react-apigallery.netlify.app/"
       />
 
-      <Project
-        title="E-commerce: WWR Headphones"
-        image="/static/projects/p8.png"
-        repo={false}
-        description="A Modern Full Stack E-Commerce React Application with Stripe"
-        tech={['React', 'NextJS', 'Sanity', 'Stripe API']}
-        frontend="https://github.com/wrobersone/e-commerce_stripe"
-        backend="https://www.sanity.io/manage"
-        video={false}
-        website="https://e-commerce-stripe.vercel.app/"
-      />
+      
     </>
   );
 };
